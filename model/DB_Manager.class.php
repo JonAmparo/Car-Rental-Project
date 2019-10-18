e4655844cf8689976e7e5813eb5717eefcbf682b
JonAmparo <jonathanmamparo@gmail.com>
2019-10-18 12:11:45 -0400

    heroku cleardb

--- model/DB_Manager.class.php
+++ model/DB_Manager.class.php
@@ -7,16 +7,48 @@ include "Invoice.class.php";
 include "Reservation.class.php";		
 include "Rental.class.php";	
 include "ReturnRental.class.php";
-
 include "Dashboard.class.php";
+
+	$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+	$cleardb_server   = $cleardb_url["host"];
+	$cleardb_username = $cleardb_url["user"];
+	$cleardb_password = $cleardb_url["pass"];
+	$cleardb_db = substr($cleardb_url["path"],1);
+
+	$active_group = 'default';
+	$query_builder = TRUE;
+
+	$db['default'] = array(
+		'dsn'    => '',
+		'hostname' => $cleardb_server,
+		'username' => $cleardb_username,
+		'password' => $cleardb_password,
+		'database' => $cleardb_db,
+		'dbdriver' => 'mysqli',
+		'dbprefix' => '',
+		'pconnect' => FALSE,
+		'db_debug' => (ENVIRONMENT !== 'production'),
+		'cache_on' => FALSE,
+		'cachedir' => '',
+		'char_set' => 'utf8',
+		'dbcollat' => 'utf8_general_ci',
+		'swap_pre' => '',
+		'encrypt' => FALSE,
+		'compress' => FALSE,
+		'stricton' => FALSE,
+		'failover' => array(),
+		'save_queries' => TRUE
+	);
+
+
 
 class DB_Manager {
 
-	const HOST = "localhost";
-	const USER = "root";
-	const PASS = "";
-	const DBNAME = "phpcarproject";
-	
+	// const HOST = "localhost";
+	// const USER = "root";
+	// const PASS = "";
+	// const DBNAME = "phpcarproject";
+
 	private $db;
 
 	public function __construct()	{
